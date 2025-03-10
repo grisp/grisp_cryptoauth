0fbc6a2f5dc1a21470319d04095667b12b8bb908
Sébastien Merle <sebastien.merle@stritzinger.com>
2025-03-10 15:46:05 +0100

    Add retry mechanism for API server calls (#10)

--- src/grisp_cryptoauth_api_server.erl
+++ src/grisp_cryptoauth_api_server.erl
@@ -2,6 +2,8 @@
 
 -behaviour(gen_server).
 
+-include_lib("kernel/include/logger.hrl").
+
 -export([start_link/0]).
 
 -export([init/1,
@@ -27,11 +29,28 @@ handle_call({Fun, Args}, _From, {Context, OldTRef}) ->
     %% to sleep after SLEEP_TIME_SEC seconds to save energy
     timer:cancel(OldTRef), %% this doesn't throw on bad args
     {ok, NewTRef} = timer:apply_after(?SLEEP_TIME_SEC * 1000, grisp_cryptoauth, sleep, [Context]),
-    try apply(grisp_cryptoauth, Fun, [Context | Args]) of
-        Result -> {reply, {ok, Result}, {Context, NewTRef}}
-    catch
-        C:R:S -> {reply, {error, C, R, S}, {Context, NewTRef}}
-    end.
+    {reply, retry(5, 300, Fun, [Context | Args]), {Context, NewTRef}}.
 
 handle_cast(_, State) ->
     {noreply, State}.
+
+retry(RetryCount, SleepTime, Fun, Args) ->
+    try apply(grisp_cryptoauth, Fun, Args) of
+        {error, _} = Error when RetryCount =< 1 ->
+            Error;
+        {error, _} = Error ->
+            ?LOG_WARNING("Error while calling function ~w [~w] ~p",
+                         [Fun, RetryCount - 1, Error]),
+            timer:sleep(SleepTime),
+            retry(RetryCount - 1, SleepTime, Fun, Args);
+        Result ->
+            {ok, Result}
+    catch
+        C:R:S when RetryCount =< 1 ->
+            {error, C, R, S};
+        Class:Reason ->
+            ?LOG_WARNING("Exception while calling function ~w [~w]: ~w:~p",
+                         [Fun, RetryCount - 1, Class, Reason]),
+            timer:sleep(SleepTime),
+            retry(RetryCount - 1, SleepTime, Fun, Args)
+    end.
